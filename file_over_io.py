28c683294e725f72b14617b971224f8d56df7016
Dealga McArdle <tetha.z@gmail.com>
2015-11-14 15:25:46 +0100

    removed osc refs in docs, renamed the sublime script 2

---
+++ file_over_io.py
@@ -0,0 +1,24 @@
+'''
+file_over_osc.py
+author: Dealga McArdle, 2015
+
+'''
+
+import time
+import sys
+import os.path
+
+import sublime
+import sublime_plugin
+
+TEMP_PATH = '/home/zeffii/Desktop/IO/fp.io'
+
+
+class FileOverOsc(sublime_plugin.TextCommand):
+    def run(self, edit):
+        view = self.view
+        file_name = view.file_name()
+        print(file_name)
+
+        with open(TEMP_PATH, 'w') as f:
+            f.write(file_name)
